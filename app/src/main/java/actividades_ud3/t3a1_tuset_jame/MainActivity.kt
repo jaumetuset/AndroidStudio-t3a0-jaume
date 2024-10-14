7c31cfff19996ffa02c2f7a2b3d4561dbb80cb05
jaumetuset <indianajaume@gmail.com>
2024-10-14 11:26:38 +0200

    Activitat 1 ud 3 - tortilla de patata con cebolla

---
+++ app/src/main/java/actividades_ud3/t3a1_tuset_jame/MainActivity.kt
@@ -0,0 +1,20 @@
+package actividades_ud3.t3a1_tuset_jame
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.image)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
